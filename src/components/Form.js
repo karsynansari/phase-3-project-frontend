d381f1a0e98c19a0bc203170ca688c7732af2941
Danielle Bencze <daniellebencze@live.com>
2022-03-09 14:13:02 -0800

    form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,47 @@
 import React, {useState } from 'react'
 import PosesDropdown from './PosesDropdown'
 
+const newYogaClassObj = {
+  teacher: "",
+  nameOfClass: "",
+  classTime: ""
+};
 
-function handleChange (e) {
-  console.log(e.target)
-}
+function Form({ poses }) {
+  const [newYogaClass, setNewYogaClass] = useState(newYogaClassObj);
 
-function handleSubmit(e) {
-  e.preventDefault()
-  console.log('Clicked')
-} 
+  function handleChange(e) {
+    // console.log(e.target)
+    setNewYogaClass((newYogaClassState) => ({
+      ...newYogaClassState,
+      [e.target.name]: e.target.value,
+    }));
+  }
 
-function Form({poses}) {
-  console.log(poses)
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log('Clicked')
+    fetch("http://localhost:9292/yoga_classes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newYogaClass),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        setNewYogaClass((existingYogaClasses) => [...existingYogaClasses, data])
+      );
+
+    setNewYogaClass(newYogaClassObj);
+  }
+
+  // console.log(poses)
+
   return (
     <div>
       <form
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
         className="form"
       >
         <div>
@@ -27,7 +52,7 @@ function Form({poses}) {
             className="form-item"
             type="text"
             name="teacher"
-            // value={}
+            value={newYogaClass.teacher}
             placeholder="Teacher's name.."
             onChange={handleChange}
           />
@@ -36,14 +61,30 @@ function Form({poses}) {
           <input
             className="form-item"
             type="text"
-            name="className"
-            // value={}
+            name="nameOfClass"
+            value={newYogaClass.nameOfClass}
             placeholder="Class name.."
             onChange={handleChange}
           />
         </div>
+        <div>
+          <input
+            className="form-item"
+            type="text"
+            name="classTime"
+            value={newYogaClass.classTime}
+            placeholder="Class time.."
+            onChange={handleChange}
+          />
+        </div>
         <PosesDropdown />
-        <input className="form-submit" type="submit" value="Submit" placeholder="Create your Class!" onSubmit = {handleSubmit}/>
+        <input
+          className="form-submit"
+          type="submit"
+          value="Submit"
+          placeholder="Create your Class!"
+          onSubmit={handleSubmit}
+        />
       </form>
     </div>
   );
